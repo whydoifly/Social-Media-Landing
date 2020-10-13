24b947ccab63c042fbb708580932d1c497e59aca
whydoifly <60507914+whydoifly@users.noreply.github.com>
2020-10-13 14:17:05 +0300

    final_look

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+var secondScene = document.getElementById('second-scene');
+var secondParallax = new Parallax(secondScene);
+
+var thirdScene = document.getElementById('third-scene');
+var thirdParallax = new Parallax(thirdScene);
+
+var fourthScene = document.getElementById('fourth-scene');
+var fourthParallax = new Parallax(fourthScene);
+
+var fifthScene = document.getElementById('fifth-scene');
+var fifthParallax = new Parallax(fifthScene);
